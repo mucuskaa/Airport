4dd1b5ecad0a4ac435df0d8d2645b19102ac3c04
Kateryna Popenko <kapopenko@gmail.com>
2023-09-07 19:42:51 +0300

    Добавьте файлы проекта.

---
+++ Airport/Plane.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport
+{
+    internal class Plane
+    {
+        public string Destination { get; set; }
+        public int FlightNumber { get; set; }
+        public string TypeOfPlane { get; set; }
+
+        public Plane(string destination, int flightNumber, string typeOfPlane) {
+            Destination = destination;
+            FlightNumber = flightNumber;
+            TypeOfPlane = typeOfPlane;
+        }
+    }
+}
---
+++ Airport/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Airport
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("---AIRPORT---");
+
+
+            Console.WriteLine("Enter count of flights");
+            int countOfPlanes;
+            while (!int.TryParse(Console.ReadLine(), out countOfPlanes))
+            {
+                Console.WriteLine("Incorrect input");
+            }
+            Plane[] planes = new Plane[countOfPlanes];
+
+            for (int i = 0; i < planes.Length; i++)
+            {
+                int flightNumber;
+                Console.WriteLine($"Enter destination of {i + 1} flight");
+                string destination = Console.ReadLine();
+
+                Console.WriteLine($"Enter number of {i + 1} flight");
+                while(!int.TryParse(Console.ReadLine(), out flightNumber))
+                {
+                    Console.WriteLine("Incorrect input");
+                }
+
+                Console.WriteLine($"Enter type of {i + 1} plane");
+                string typeOfPlane = Console.ReadLine();
+
+                planes[i] = new Plane(destination, flightNumber, typeOfPlane);
+            }
+
+            Airports airport = new Airports(planes);
+            Console.WriteLine("Enter destination to search:");
+            string searchDestination = Console.ReadLine();
+            Plane foundPlane = airport.FindByDestination(searchDestination);
+
+            if (foundPlane != null)
+            {
+                Console.WriteLine("Found plane:");
+                Console.WriteLine($"Destination: {foundPlane.Destination}");
+                Console.WriteLine($"Flight Number: {foundPlane.FlightNumber}");
+                Console.WriteLine($"Type of Plane: {foundPlane.TypeOfPlane}");
+            }
+            else
+            {
+                Console.WriteLine("No plane found with the specified destination.");
+            }
+
+        }
+    }
+}
